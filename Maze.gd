24a57da58f1484b44350db08cbbbf22ccc6db13f
Nathaniel Mattsson <boomknife@lysator.liu.se>
2026-02-27 21:20:04 +0100

    Floor instantiates

---
+++ Maze.gd
@@ -0,0 +1,41 @@
+extends Area2D
+
+const height = 10
+const width = 10
+var board = []
+var sprites = []
+const tileSize = Vector2i(32,32)
+
+enum Tile {WALL, FLOOR, MOUSE, PLAYER}
+
+func _ready():
+	for y in range(0,height):
+		for x in range(0,height):
+			board.append(Tile.FLOOR)
+	addSprites()
+			
+func getIndex(x, y):
+	return y * width + x
+	
+func getLoc(index):
+	var y = int(index)/int(width)
+	var x = index % width
+	return Vector2i(x,y)
+	
+func addSprites():
+	for i in range(board.size()):
+		match board[i]:
+			Tile.WALL:
+				break
+			Tile.FLOOR:
+				var sprite = Sprite2D.new()
+				sprite.texture = load("res://assets/mazetile.png")
+				var location = getLoc(i)*tileSize
+				print(i,location,board.size())
+				sprite.global_position = getLoc(i)*tileSize
+				add_child(sprite)
+				sprites.append(sprite)				
+			Tile.MOUSE:
+				break
+			Tile.PLAYER:
+				break
